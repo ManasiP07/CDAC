ff238431c83000128fa4fb9f09aa1ee2b3f46f79
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:32:42 +0530

    Update Q.3.java

--- Logic Building/Assignments/Assignment1/Q.3.java
+++ Logic Building/Assignments/Assignment1/Q.3.java
@@ -1,1 +1,13 @@
+//odd or even
+public class Aug20_Q3 {
+    public static void main(String args[]) {
+        int num = 13;
+        if (num % 2 == 0) {
+            System.out.println("Given number is even number");
+        } else {
+            System.out.println("Given number is odd number");
+        }
+    }
+
+}
 
