93debb5caa3e1ef179e5121f1c189b5fdc83901c
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:31:31 +0530

    Update Q.1.java

--- Logic Building/Assignments/Assignment1/Q.1.java
+++ Logic Building/Assignments/Assignment1/Q.1.java
@@ -1,1 +1,9 @@
-
+//Even number
+public class Aug20_Q1 {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num % 2 == 0) {
+            System.out.println("Given number is Even");
+        }
+    }
+}
