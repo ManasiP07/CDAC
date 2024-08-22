748ca1d29042d1c2113e1de7b5c96c714a1122db
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:41:44 +0530

    Update Q.6.java

--- Logic Building/Assignments/Assignment1/Q.6.java
+++ Logic Building/Assignments/Assignment1/Q.6.java
@@ -1,1 +1,14 @@
+/*Write a Java program that calculates the area of a rectangle using the formula
+area = length * width. Use predefined values for length and width. 
+*/
+public class Aug20_Q6 {
+    public static void main(String[] args) {
+        int length = 12;
+        int width = 15;
+        int area_of_rectangle;
+        area_of_rectangle = length * width;
+        System.out.println("Area of rectangle is:" + area_of_rectangle);
+    }
+
+}
 
