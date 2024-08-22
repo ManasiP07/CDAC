6440a4a4ab7196d98fb1095bb1c8c615591e1eab
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:42:07 +0530

    Update Q.7.java

--- Logic Building/Assignments/Assignment1/Q.7.java
+++ Logic Building/Assignments/Assignment1/Q.7.java
@@ -1,1 +1,20 @@
+/*Write a Java program that finds and prints the largest of three predefined
+numbers using if-else statements. */
+public class Aug20_Q7 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = 5;
+        if (a > b && a > c) {
+            System.out.println("a is largest");
+        } else if (b > c) {
+            System.out.println("b is largest");
+        }
+
+        else {
+            System.out.println("c is largest");
+        }
+    }
+
+}
 
