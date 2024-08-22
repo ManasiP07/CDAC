decc83b0feef8661d9fa026e8d1721e3051be932
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:31:58 +0530

    Update Q.2.java

--- Logic Building/Assignments/Assignment1/Q.2.java
+++ Logic Building/Assignments/Assignment1/Q.2.java
@@ -1,1 +1,10 @@
+//odd number
+public class Aug20_Q2 {
+    public static void main(String[] args) {
+        int num = 13;
+        if (num % 2 != 0) {
+            System.out.println("Given number is odd");
+        }
 
+    }
+}
