ddf2147c374dc0dbf6f02292e4c42bc46c7160e1
Manasi Patil <134922503+ManasiP07@users.noreply.github.com>
2024-08-22 19:40:18 +0530

    Update Q.4.java

--- Logic Building/Assignments/Assignment1/Q.4.java
+++ Logic Building/Assignments/Assignment1/Q.4.java
@@ -1,1 +1,15 @@
+/*Write a Java program that displays a "Good Morning" message if the
+predefined time is between 5 AM and 12 PM. Use an if statement to implement the logic. */
+public class Aug20_Q4 {
+    public static void main(String[] args) {
+        int time = 6;
+        String period="AM";
 
+        if (period="AM" && 12 >= time && time >= 5) {
+            System.out.println("GOOD MORNING!");
+        }
+      else{
+        System.out.println("It is not a morning");
+    }
+
+}
